1dae7c99489eb8b81d961bce08da23236942661a
Rahul Thakur <surya727859@gmail.com>
2026-04-20 00:12:11 +0530

    fixes: deployement issue

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL||"expense-tracker-backend-chi-green.vercel.app/api"   //temporary heard code due to deployment issue
+,
   headers: { "Content-Type": "application/json" },
 });
 
